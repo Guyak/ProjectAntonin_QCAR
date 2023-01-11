399d5865421aca32fd794eb3d9ee4629eac76f6d
Guyak <guyotantonin2001@gmail.com>
2023-01-11 09:11:03 +0100

    path follow completion (not done)

--- Bicycle_model/PeterCorke/BicyclePeterCorkePathFollowSetup.m
+++ Bicycle_model/PeterCorke/BicyclePeterCorkePathFollowSetup.m
@@ -4,6 +4,9 @@ open_system('BicyclePeterCorkePathFollow.slx')
 
 initCoord = [0 0 0];
 path = butterfly(8,8);
+%path = circle(0,0,1);
+
+plot(path(:,1), path(:,2), '*')
 
 speedLim = 1;
 accelLim = 1;
@@ -14,10 +17,13 @@ simulation = sim('BicyclePeterCorkePathFollow.slx');
 position = simulation.position.Data;
 angle = simulation.angle.Data;
 
+figure()
 plot(position(:,1), position(:,2))
 hold on
 plot(initCoord(1), initCoord(2), '*r')
 text(initCoord(1), initCoord(2), 'Start');
+xlim([-8 8])
+ylim([-8 8])
 
 function p=butterfly(width, height)
     freq = 2*pi/30;
@@ -28,4 +34,13 @@ function p=butterfly(width, height)
 %     plot(xRef, yRef, "+");
 %     title('Butterfly');
     p=[0 xRef ; 0 yRef]';
+end
+
+function p=circle(x,y,r)
+%create and plot a circle with radius r and origin (x,y)
+    ang=(2*pi/64):(2*pi/64):2*pi; 
+    xp=r*cos(ang);
+    yp=r*sin(ang);
+    plot(x+xp,y+yp,"+");
+    p=[xp;yp]';
 end
